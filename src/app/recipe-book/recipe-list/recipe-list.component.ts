54d00d9cf406ea853970945e874cb2f14ad78a46
Derek Ely <derekely21@gmail.com>
2023-03-04 14:32:43 -0700

    filled in more basics and seperated recipies/list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] =  [
+    new Recipe("Test Recipe", "Wow a test recipe!!", "https://freesvg.org/img/1533577397.png"),
+    new Recipe("Test Recipe", "Wow a test recipe!!", "https://freesvg.org/img/1533577397.png")
+  ];
 
+  constructor(){ }
+  
+  ngOnInit(){
+
+  }
 }
